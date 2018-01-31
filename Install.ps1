cab361fc07ab368451219006962e072b4675e50e
roman-yagodin <roman.yagodin@gmail.com>
2018-01-31 16:18:11 +0300

    Rebranding, add missing containers and basic script to generate install package

---
+++ Install.ps1
@@ -0,0 +1,19 @@
+$PackageName="DnnContra.LegalShield"
+$Version="00.01.00"
+
+Push-Location
+
+New-Item -Path "tmp" -ItemType Directory -Force
+
+Compress-Archive -Path "./*" -DestinationPath "Resources_Skins.zip"
+Compress-Archive -Path "./containers/*" -DestinationPath "Resources_Containers.zip"
+
+Copy-Item -Path "./*" -Destination "tmp" -Recurse -Include "*.txt", "*.dnn", "*.zip"
+
+Remove-Item -Path "Resources_Skins.zip"
+Remove-Item -Path "Resources_Containers.zip"
+
+Set-Location -Path "tmp"
+Compress-Archive -Path "./*" -DestinationPath "$PackageName-$Version-Install.zip"
+
+Pop-Location
