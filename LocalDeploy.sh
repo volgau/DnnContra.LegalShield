d0ff9e5ab507c316f1fd65a5e95b53f81dc035a4
roman-yagodin <roman.yagodin@gmail.com>
2018-02-06 16:57:09 +0300

    Close GH-6: Add local deploy script

---
+++ LocalDeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PACKAGE_NAME="DnnContra.LegalShield"
+DEPLOY_PATH="$HOME/mnt/dnn8"
+SKINS_DEPLOY_PATH="$DEPLOY_PATH/Portals/_default/Skins/$PACKAGE_NAME"
+CONTAINERS_DEPLOY_PATH="$DEPLOY_PATH/Portals/_default/Containers/$PACKAGE_NAME"
+
+rsync --exclude ".git" -rv "./" "$SKINS_DEPLOY_PATH"
+rsync -rv "./containers/" "$CONTAINERS_DEPLOY_PATH"
