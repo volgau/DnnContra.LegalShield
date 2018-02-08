29b2269a758fa52817325780d43964bdc50b7c37
roman-yagodin <roman.yagodin@gmail.com>
2018-02-08 11:47:36 +0300

    Exclude tmp directory from local deploy

--- LocalDeploy.sh
+++ LocalDeploy.sh
@@ -5,5 +5,5 @@ DEPLOY_PATH="$HOME/mnt/dnn8"
 SKINS_DEPLOY_PATH="$DEPLOY_PATH/Portals/_default/Skins/$PACKAGE_NAME"
 CONTAINERS_DEPLOY_PATH="$DEPLOY_PATH/Portals/_default/Containers/$PACKAGE_NAME"
 
-rsync --exclude ".git" -rv "./" "$SKINS_DEPLOY_PATH"
+rsync --exclude ".git" --exclude "tmp" -rv "./" "$SKINS_DEPLOY_PATH"
 rsync -rv "./containers/" "$CONTAINERS_DEPLOY_PATH"
